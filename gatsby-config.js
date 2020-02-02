233774f150aee62deebaff779ce7a6ad77362190
Jeremiah Billmann <jbillmann@gmail.com>
2020-02-01 17:45:55 -0700

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,75 @@
+require(`dotenv`).config({
+  path: `.env`
+});
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: "Jeremiah Billmann",
+    siteTitleAlt: `Jeremiah Billmann`
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`
+          },
+          {
+            title: `About`,
+            slug: `/about`
+          }
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/jbilmann`
+          },
+          {
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/jeremiah-billmann-86393117`
+          },
+          {
+            name: `Github`,
+            url: `https://www.github.com/jbillmann`
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `A Blog By Jeremiah Billmann`,
+        short_name: `Jeremiah Billmann Blog`,
+        description: `Software engineer most of the time.  Beer aficionado, 60% of the time, all the time.  And this is my blog some of the time.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`
+          }
+        ]
+      }
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
+  ]
+};
---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/blog-list-item.tsx
@@ -0,0 +1,50 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx, Styled } from "theme-ui";
+import { Box } from "@theme-ui/components";
+import { Link } from "gatsby";
+import moment from "moment";
+import ItemTags from "./item-tags";
+
+type BlogListItemProps = {
+  post: {
+    slug: string;
+    title: string;
+    date: string;
+    tags?: {
+      name: string;
+      slug: string;
+    }[];
+  };
+  showTags?: boolean;
+};
+
+const BlogListItem = ({ post, showTags = true }: BlogListItemProps) => (
+  <Box mb={4}>
+    <Styled.a
+      as={Link}
+      to={post.slug}
+      sx={{ fontSize: [1, 2, 3], color: `text` }}
+    >
+      {post.title}
+    </Styled.a>
+    <p
+      sx={{
+        color: `secondary`,
+        mt: 1,
+        a: { color: `secondary` },
+        fontSize: [1, 1, 2]
+      }}
+    >
+      <time>{moment(post.date, "DD.MM.YYYY").format("DD MMM YYYY")}</time>
+      {post.tags && showTags && (
+        <React.Fragment>
+          {` — `}
+          <ItemTags tags={post.tags} />
+        </React.Fragment>
+      )}
+    </p>
+  </Box>
+);
+
+export default BlogListItem;
